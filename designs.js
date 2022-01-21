d2dc21214c6edeb66b96e9633c58e230781bf1fc
mbMAV <98023951+mbMAV@users.noreply.github.com>
2022-01-21 18:48:47 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
 // Select color input
 // Select size input
+    var col = document.getElementById("colorPicker").value;
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    var high = document.getElementById("inputHeight").value;
+    var wide = document.getElementById("inputWidth").value;
+    var body = document.getElementsByTagName("body")[0];
+    var table = document.getElementById("pixelCanvas");
+    var tBody = document.createElement("tbody")
 
-// Your code goes here!
 
+
+for (var i = 0; i < wide; i++) {
+    var row = document.createElement("tr");
+    tBody.appendChild(row);
+    
+    for (var j = 0; j < high; j++); {
+        var column = document.createElement("td");
+        row.appendChild(column);
+    }
+    tBody.appendChild(column)
+    
 }
+
+table.appendChild(tBody);
+document.body.appendChild(table);
+
+tBody.addEventListener('mousedown', function (event) {
+    event.target.style.backgroundColor = col.value;
+})
+
+event.preventDefault();
+};
